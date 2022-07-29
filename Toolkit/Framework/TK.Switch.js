bac93edb06ef3c2df4cc1ca4213f5bbf0685f501
Comgenie <comgenie@comgenie.com>
2022-07-29 19:38:46 +0200

    Add switch form element

---
+++ Toolkit/Framework/TK.Switch.js
@@ -0,0 +1,52 @@
+﻿"use strict";
+window.TK.Switch = {
+    TextBefore: "",
+    TextAfter: "",
+    Data: false,
+    className: "toolkitSwitch",
+
+    onchange: function () { },
+
+    onclick: function () {
+        this.Toggle();
+    },
+    Toggle: function () {
+        this.Data = !this.Data;
+        this.Init();
+    },
+    Init: function () {
+        if (this.DataSettings) {
+            if (this.DataSettings.TextBefore !== undefined)
+                this.TextBefore = this.DataSettings.TextBefore;
+            if (this.DataSettings.TextAfter !== undefined)
+                this.TextAfter = this.DataSettings.TextAfter;
+        }
+
+        this.className = this.className.replace(/toolkitSwitchActive/g, "").replace(/toolkitSwitchInactive/g, "") + " " + (this.Data ? "toolkitSwitchActive" : "toolkitSwitchInactive");
+        this.Elements.TextBefore.innerText = this.TextBefore;
+        this.Elements.TextBefore.style.display = this.TextBefore ? "" : "none";
+        this.Elements.TextAfter.innerText = this.TextAfter;
+        this.Elements.TextAfter.style.display = this.TextAfter ? "" : "none";
+    },
+    GetValue: function () {
+        return this.Data;
+    },
+    Elements: {
+        TextBefore: {},
+        SwitchContainer: {
+            Elements: {
+                Indicator: {
+                    _: "div"
+                }
+            }
+        },
+        TextAfter: {},
+    }
+};
+
+
+if (window.TK.Form) {
+    window.TK.Form.DefaultTemplates.switch = {
+        _: TK.Switch
+    };
+}
